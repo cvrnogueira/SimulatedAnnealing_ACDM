8e69b737764b9724f550ba72941a1d2070c2abbb
Catarina <caca_rosenau@hotmail.com>
2019-06-21 00:55:32 -0300

    now i've placed functions into SA algoritm

--- simulatedAnnealing.py
+++ simulatedAnnealing.py
@@ -21,7 +21,7 @@ def checkFeasibility(P, s):
 
 def getInitialSolution(P):
     #Add descending order because we think this way the initial solution will be better
-    order_descending = P.sort(reverse=True)
+    P.sort(reverse=True)
     #start s array with -1
     s = [-1] * len(P)
     #Create the first and second si's because they are always the same
@@ -59,34 +59,40 @@ def get_instance(filename):
 
     return int(instance_line_1[0]), weights
 
-def learn(N, P) -> list:
-        sa = SimulatedAnnealing(size= N, weights = P, temperature=100.0, dec_temperature=0.92, num_iterations=N, max_changes=4)
-        #sa.fitness_function = self.run_episode
-        sa.run()
-
+def learn() -> list:
+        get_instance_on_file = get_instance("instances/teste.dat")
+        initialSolution = getInitialSolution(get_instance_on_file[1])
+        feasibility_status = checkFeasibility(initialSolution[0], initialSolution[1])
+        if feasibility_status:
+            sa = SimulatedAnnealing(initialSolution= initialSolution, temperature=100.0, dec_temperature=0.92, num_iterations=len(initialSolution[0]), max_changes=4)
+            sa.run()
+        else:
+            print("ERROR: Initial solution not feasible!!!")
+            return [-1]
         return sa.actual_state
 
 
 class SimulatedAnnealing:
 
-    def __init__(self, weights, size, temperature, dec_temperature, num_iterations, max_changes=4):
+    def __init__(self, initialSolution, temperature, dec_temperature, num_iterations, max_changes=4):
         self.actual_state = []
-        self.size = size
         self.temperature = temperature
         self.dec_temperature = dec_temperature
         self.num_iterations = num_iterations
         self.max_changes = max_changes
         self.inc_neighbor = 2
-        self.initial_state = weights
+        self.initial_state = initialSolution
         self.score = 0;
         self.max_value = 10;
         self.min_value =  -10;
 
     #TODO: HEEELP
     #Como função de fitness vou somar a duração das tarefas com a solução dada
-    def fitness_function(self, solution):       
-        value = sum(solution) 
-        return value
+    def getFitness(self, P,s):
+        sums = []
+        for i in range(len(P)):
+           sums.append(P[i] + s[i])
+        return max(sums)
 
     def update_temperature(self):
         self.temperature *= self.dec_temperature
@@ -111,16 +117,18 @@ class SimulatedAnnealing:
 
 
     def run(self):
-        #Solução inicial : exato como foi a leitura do arquivo
-        self.actual_state_copy = self.initial_state
+        #initial_state: Tuple (P, s)
+        self.actual_state_copy = self.initial_state 
 
+        print(self.actual_state_copy)
+        '''
         while self.temperature > 0.01:
             self.update_temperature()
             for i in range(self.num_iterations):
                 candidate = self.get_random_neighbor(self.actual_state_copy.copy())
 
-                candidate_score = self.fitness_function(candidate)
-                actual_state_score = self.fitness_function(self.actual_state_copy)
+                candidate_score = self.getFitness(candidate)
+                actual_state_score = self.getFitness(self.actual_state_copy)
 
                 delta = candidate_score - actual_state_score
                 self.score = actual_state_score
@@ -134,13 +142,8 @@ class SimulatedAnnealing:
                         self.actual_state_copy = candidate
                         self.score = candidate_score
         print(self)
-
+        '''
     def __str__(self):
         return 'Estado da solução = \n' + str(["{0:0.2f}".format(i) for i in self.actual_state_copy]) + '\n Valor encontrado para T = \n' + str(self.score) + ' \n Solução inicial = \n' + str(self.initial_state);
 
-get_instance_on_file = get_instance("instances/teste.dat")
-initialSolution = getInitialSolution(get_instance_on_file[1])
-feasibility_status = checkFeasibility(initialSolution[0], initialSolution[1])
-#if not(feasibility_status):
-print(feasibility_status)
-#learn(get_instance_on_file[0], get_instance_on_file[1])
+learn()
